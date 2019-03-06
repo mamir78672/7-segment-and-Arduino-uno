7f445219f2a87da21b89054da8b5e7b33999ef4d
Muhammad Amir <48288115+mamir78672@users.noreply.github.com>
2019-03-06 20:50:56 +0500

    Add files via upload
    Design a Circuit on Proteus In which seven segment is controlled by Arduino uno and Make  up and down counters using seven segment and Arduino.  As we know seven segment display is an electronic device used for displaying the numeric data.

---
+++ Code.ino
@@ -0,0 +1,59 @@
+int num_Array[16][7] = {  { 1,1,1,1,1,1,0 },
+                          { 0,1,1,0,0,0,0 },
+                          { 1,1,0,1,1,0,1 },
+                          { 1,1,1,1,0,0,1 },
+                          { 0,1,1,0,0,1,1 },
+                          { 1,0,1,1,0,1,1 },
+                          { 1,0,1,1,1,1,1 },
+                          { 1,1,1,0,0,0,0 },
+                          { 1,1,1,1,1,1,1 },
+                          { 1,1,1,0,0,1,1 },
+                          { 1,1,1,0,1,1,1 },
+                          { 0,0,1,1,1,1,1 },
+                          { 1,0,0,1,1,1,0 },
+                          { 0,1,1,1,1,0,1 },
+                          { 1,0,0,1,1,1,1 },
+                          { 1,0,0,0,1,1,1 },
+                        };
+void disp_Num(int);
+void dispOFF();
+void setup() 
+{ 
+  pinMode(0, OUTPUT);   
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+void loop() 
+{ 
+  for(int count = 0;count<=15;count++)
+  {
+    disp_Num(count);
+    dispOFF();
+  }
+  for (int count = 15; count>=0; count--) 
+  {
+   dispOFF(); 
+   disp_Num(count);
+  }
+  dispOFF();
+}  
+void disp_Num(int number) 
+{
+  for (int i=0; i<=6; i++) 
+  {
+   digitalWrite(i,num_Array[number][i]);
+  }
+}
+void dispOFF()
+{
+  delay(500);
+  for (int i=0; i<=6; i++) 
+  {
+   digitalWrite(i,LOW);
+  }
+}
+
